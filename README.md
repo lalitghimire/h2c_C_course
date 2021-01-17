1bb9e992508b8135cbebecb5f55d49a6b51224ee
Lalit Ghimire <lalitghimire@gmail.com>
2021-01-17 19:43:47 +0200

    part two done

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # h2c_C_course
-Rock paper scissor game in 5 part
+
+Rock paper scissor game in 5 part.
+
+Instructions as given in exercises
+
+Rock-Paper-Scissors, part 1
+
+- Write a main for the rock-paper-scissors game. First, print a welcoming message to the screen.
+- In main, define four variables (type: int) called wins, computerWins, computersChoice and rounds.
+- Ask the user how many rounds she wishes to play (use rounds-variable)
+- In main, initialize random number generation: srand(time(0)); Remember to include <time.h>
+- Then, generate a random number between 1 and 3: rand() % 3 + 1. Set the value to the computerChoice variable.
+- Print the value of computersChoice to the screen.
+
+Rock-Paper-Scissors, part 2
+
+- Print the value of computersChoice as text to the screen using an if-clause. (1=rock, 2=paper, 3=scissors)
+- Define new variables called userChoiceAsChar (char) and userChoiceAsInt (int)
+- Ask the user's choice using printf and scanf. R or r = rock, P or p = paper, S or s = scissors
+- Using if, set the userChoiceAsInt to a correct value (1, 2 or 3)
+- Print the value of userChoiceAsInt
--- rock_paper_scissor.c
+++ rock_paper_scissor.c
@@ -7,20 +7,64 @@ int main()
 
     int wins, computerWins, computerChoice, rounds;
 
-    //print greetings message//
+    //print greetings message
     printf("Welcome to the Rock-Paper-Scissor game!!! \n");
 
-    //ask user no. of rounds to play, take input and print their choice//
+    //ask user no. of rounds to play, take input and print their choice
     printf("How many rounds you wish to play? \n");
     scanf("%d", &rounds);
     printf("You chose to play %d rounds!!\n", rounds);
 
-    //initialize random number generation function//
+    //initialize random number generation function
     srand(time(0));
 
-    //random number generation between 1 and 3 for computer choice and print computer choice//
+    //random number generation between 1 and 3 for computer choice and print computer choice
     computerChoice = rand() % 3 + 1;
     printf("Computer choice is %d", computerChoice);
 
-    //******************end of exercise from part 1*********************//
+    //******************End of exercises from part 1*********************//
+
+    //--------------------part2------------------------------------------//
+
+    // printing value of computer choice as text instead of number//
+    if (computerChoice == 1)
+    {
+        printf("Computer choice is rock!!");
+    }
+    else if (computerChoice == 2)
+    {
+        printf("Computer choice is paper!!");
+    }
+    else
+    {
+        printf("Computer choice is scissor!!");
+    }
+
+    //declaring variables from part 2//
+    int userChoiceAsInt;
+    char userChoiceAsChar;
+
+    //Asking user for input to play the game//
+    printf("\nChoose R or r for rock, P or p for paper and S or s for scissors\n");
+    scanf(" %c", &userChoiceAsChar);
+
+    // Setting the userchoice to value
+    if (userChoiceAsChar == 'R' || userChoiceAsChar == 'r')
+    {
+        userChoiceAsInt = 1;
+    }
+    else if (userChoiceAsChar == 'P' || userChoiceAsChar == 'p')
+    {
+        userChoiceAsInt = 2;
+    }
+    else if (userChoiceAsChar == 'S' || userChoiceAsChar == 's')
+    {
+        userChoiceAsInt = 3;
+    }
+
+    printf("User choice is: %d", userChoiceAsInt); // printing the value of userchoice
+
+    //******************End of exercises from part 2*********************//
+
+    //--------------------part3------------------------------------------//
 }
